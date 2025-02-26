0d9a32521aa4fe6e7b86c85282da21ad99043240
Minh Nguyen <mnguyen6@unb.ca>
2025-02-26 16:30:45 -0400

    real time testing
    I created a Python script to make model inferences on frames collected from the camera. the result of this real-time is fairly bad. The model was not able to detect a face properly. This might be because therer is a difference in the environment of collected  data and test environment.
    
    Again, this is just a preliminary test to check if the pipeline works. Will need to do further training for any meaningful model

--- face_detection_CNN.py
+++ face_detection_CNN.py
@@ -1,48 +1,49 @@
-import os 
-import time
-import uuid     # To create nique uniform identifier for collected images
-import cv2      # For access camera sensors for image data collection
+from tensorflow.keras.models import load_model
+import cv2
+import tensorflow as tf
+import numpy as np
 
-IMAGES_PATH = os.path.join('data','images')
-number_images = 5
+# Load the saved model 
+facetracker = load_model('models/facetracker.h5', compile=False)
 
 cap = cv2.VideoCapture(0)
-cap.set(3, 1280)
-cap.set(4, 960)
+cap.set(cv2.CAP_PROP_EXPOSURE,-6)
 
-# for imgnum in range(number_images):
-#     print(f'Collecting image {imgnum}')
-#     ret, frame = cap.read()
-
-#     if not ret:
-#         print("There are some errors in the capture process")
-#         break
-#     else:
-#         imgname = os.path.join(IMAGES_PATH,f'{str(uuid.uuid1())}.jpg')
-#         cv2.imwrite(imgname, frame)
-#         cv2.imshow('Frame', frame)
-#         time.sleep(0.5)
-
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-#         break
-
-while True:
+while cap.isOpened():
     ret, frame = cap.read()
-
     if not ret:
-        print("Error")
-        break
-    else:
-        
-        # time.sleep(0.5)
-        print("Hello")
-        cv2.imshow("Video stream",frame)
-
-    if cv2.waitKey(1) == ord('q'):
-        print("User quit")
+        print("There are some errors in the capture process")
         break
 
+    # Ensure that the camera captures a 450:450 frame
+    frame = frame[50:500, 50:500, :]
+
+    rgb =cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    # cv2.imshow("Face Detection", frame)
+    resized = tf.image.resize(rgb,(120,120))
+    # cv2.imshow("Face Detection", resized)
+
+    yhat = facetracker.predict(np.expand_dims(resized/255,0))
+    sample_coords = yhat[1][0]
+
+    detection_threshold = 0.3
+    if yhat[0] > detection_threshold:
+        x1, y1, x2, y2 = np.multiply(sample_coords, [450, 450, 450 ,450]).astype(int)
+        print(x1, y1, x2, y2)
+
+        # cv2.rectangle(frame, (x1,y1), (x2,y2), (255,0,0), 2)
+        # cv2.rectangle(frame, (x1,y1-20), (x1+80,y1), (255,0,0), -1)
+        # cv2.putText(frame, 'face',(x1,y1-5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 2)
+
+        cv2.rectangle(frame,
+                      tuple(np.multiply(sample_coords[:2], [450, 450]).astype(int)),
+                      tuple(np.multiply(sample_coords[2:], [450, 450]).astype(int)),
+                      (255,0,0), 2)
     
+    cv2.imshow("Face Detection", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
 cap.release()
 cv2.destroyAllWindows()
