8c0ba4f305aabe6cc6d4559a56fa9c7cf49582d6
Minh Nguyen <mnguyen6@unb.ca>
2025-05-10 13:20:43 -0300

    working on TD learning with FrozenLake
    - implemented SARSA update for policy evaluation
    - implemented a custom function to plot a simplified frozen lake. However, there are multiple issues. Firstly, the values are super small because not all of the trajectories that started from the earlier states can reach a reward => modified the customized frozen lake to have negative penalties. Secondly, the policy does not yet seem to conform to the state-action values visualized (maybe an error in policy array reshaping or in the way I do np.argmax

--- DRL/gym_exercise/frozen_lake_plot.py
+++ DRL/gym_exercise/frozen_lake_plot.py
@@ -3,16 +3,17 @@ import matplotlib.pyplot as plt
 
 # Initialize environment parameters
 grid_size = 4
+n_states = 16
+n_actions = 4
 holes = [(1, 1), (1, 3), (2, 3), (3, 0)]  # Bad states
 goal = (3, 3)  # Terminal state
 start = (0, 0)
 
 # Initialize value tables
 V = {(i, j): 0 for i in range(grid_size) for j in range(grid_size)}
-Q = {(i, j): {'up': 0, 'down': 0, 'left': 0, 'right': 0} 
-      for i in range(grid_size) for j in range(grid_size)}
+Q = np.random.rand(n_actions,n_states)
 
-def plot_grid(V, Q, title="Frozen Lake"):
+def plot_grid(Q, title="Frozen Lake"):
     fig, ax = plt.subplots()
     ax.set_xticks(np.arange(grid_size+1))
     ax.set_yticks(np.arange(grid_size+1))
@@ -36,12 +37,11 @@ def plot_grid(V, Q, title="Frozen Lake"):
             ax.add_patch(plt.Rectangle((col, row), 1, 1, facecolor=bg_color, edgecolor='black'))
             
             # Text: State value (center) and Q-values (directions)
-            ax.text(col + 0.5, row + 0.5, f"{V[(row, col)]:.2f}", 
-                    ha='center', va='center')
-            ax.text(col + 0.05, row + 0.5, f"{Q[(row, col)]['left']:.2f}", fontsize=8, rotation=-90, ha='left', va='center')
-            ax.text(col + 0.95, row + 0.5, f"{Q[(row, col)]['right']:.2f}", fontsize=8, rotation=90, ha='right', va='center')
-            ax.text(col + 0.5, row + 0.2, f"{Q[(row, col)]['up']:.2f}", fontsize=8, ha='center', va='center')
-            ax.text(col + 0.5, row + 0.8, f"{Q[(row, col)]['down']:.2f}", fontsize=8, ha='center', va='center')
+            # ax.text(col + 0.5, row + 0.5, f"{V[(row, col)]:.2f}", ha='center', va='center')
+            ax.text(col + 0.5, row + 0.2, f"{Q[0,row*grid_size+col]:.3f}", fontsize=8, ha='center', va='center')
+            ax.text(col + 0.95, row + 0.5, f"{Q[1,row*grid_size+col]:.3f}", fontsize=8, rotation=90, ha='right', va='center')            
+            ax.text(col + 0.5, row + 0.8, f"{Q[2,row*grid_size+col]:.3f}", fontsize=8, ha='center', va='center')
+            ax.text(col + 0.05, row + 0.5, f"{Q[3,row*grid_size+col]:.3f}", fontsize=8, rotation=-90, ha='left', va='center')
 
 
     plt.title(title)
