ed0c3146841c91c55c439a7fcd83820f2b9a43d4
Minh Nguyen <mnguyen6@unb.ca>
2025-09-10 15:31:18 -0300

    add trained policies for swingup control, test simulate the policies, and plot action/reward of the simulation
    - only run_00003 has a good policy that can bring the pendulum up, none of the policy was able to reach early stop
    - the policies took around 5+ minutes to go through 1e6 training steps on the work computer

--- DRL/gym_exercise/InvertedPendulum/SwingUp/inv_pend_wrapper.py
+++ DRL/gym_exercise/InvertedPendulum/SwingUp/inv_pend_wrapper.py
@@ -8,7 +8,7 @@ class SwingUpInvPenWrapper(gym.Wrapper):
         self.x_threshold = 2
         self.observation_space
 
-    def reset(self, **kwargs):
+    def reset(self, train = True, **kwargs, ):
         
         # --- reset the environment and unwrap the TimeLimit wrapper
         obs, info = self.env.reset(**kwargs)
@@ -16,7 +16,7 @@ class SwingUpInvPenWrapper(gym.Wrapper):
         core = self.env.unwrapped
 
         # # --- set pendulum down initial position + noise
-        if core.np_random.random() > 0.8:   # some probability to start from pendulum up
+        if train and core.np_random.random() > 0.8:   # some probability to start from pendulum up
             qpos = np.array([0.0, 0.0]) + core.np_random.normal(0, 0.1, size=2)
         else:
             qpos = np.array([0.0, np.pi]) + core.np_random.normal(0, 0.1, size=2)
